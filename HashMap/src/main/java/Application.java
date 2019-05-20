7106a929d2892a87fb5b184da58a26c9dea21dc8
Ryus4ku <logaNero20NIKITA>
2019-05-20 22:06:42 +0500

    20.05

--- HashMap/src/main/java/Application.java
+++ HashMap/src/main/java/Application.java
@@ -5,25 +5,23 @@ import java.util.Map;
 import java.util.concurrent.*;
 
 public class Application {
-    private final static int THREAD_POOL_SIZE = 2;
-
+    private final static int THREAD_POOL_SIZE = 4;
     private static Map<String, Integer> hashMap = null;
     private static Map<String, Integer> hashTable = null;
     private static Map<String, Integer> synchronizedMap = null;
     private static Map<String, Integer> concurrentHashMap = null;
 
-
     public static void main(String[] args) throws InterruptedException {
-        hashMap = new HashMap<String, Integer>();
+        hashMap = new HashMap<>();
         testMap(hashMap);
 
-        hashTable = new Hashtable<String, Integer>();
+        hashTable = new Hashtable<>();
         testMap(hashTable);
 
-        synchronizedMap = Collections.synchronizedMap(new HashMap<String, Integer>());
+        synchronizedMap = Collections.synchronizedMap(new HashMap<>());
         testMap(synchronizedMap);
 
-        concurrentHashMap = new ConcurrentHashMap<String, Integer>();
+        concurrentHashMap = new ConcurrentHashMap<>();
         testMap(concurrentHashMap);
     }
 
@@ -36,7 +34,6 @@ public class Application {
             long startTime = System.nanoTime();
             ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-
             for (int j = 0; j < THREAD_POOL_SIZE; j++) {
                 executorService.execute (() -> {
                     for (int element = 0; element < 500000; element++) {
@@ -48,15 +45,12 @@ public class Application {
                 });
             }
             executorService.shutdown();
-
-            // Blocks until all tasks have completed execution after a shutdown request, or the timeout occurs, or the current thread is
-            // interrupted, whichever happens first.
             executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
 
             long entTime = System.nanoTime();
             long totalTime = (entTime - startTime) / 1000000L;
             averageTime += totalTime;
-            System.out.println(totalTime + " ms");
+            System.out.println(totalTime + " мс");
         }
         System.out.println("Для " + testMap.getClass() + " среднее время " + averageTime / 5 + " мс\n");
     }
--- openMP/openMP/openMP.cpp
+++ openMP/openMP/openMP.cpp
@@ -1,7 +1,4 @@
-﻿// openMP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include <time.h>
 #include <omp.h>
